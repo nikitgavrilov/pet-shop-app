18bb2dece8c22a7b00f3a659875d052df6da93b7
Niki <gavrilovnikita2004@gmail.com>
2024-05-17 18:07:15 +0300

    created-2-more-sections

--- src/pages/main/main.php
+++ src/pages/main/main.php
@@ -13,7 +13,7 @@
 		<!-- Оболочка демонстрации -->
     <div class="wrapper">
          <?php include("./../../components/header/header.php") ?>
-				 <main>
+				<main>
 					<section class="main__copilot copilot">
 							<div class="container">
 								<div class="copilot__body">
@@ -29,8 +29,8 @@
 									</div>
 								</div>
 							</div>
-						</section>
-						<section class="main__products products">
+					</section>
+					<section class="main__products products">
 							<div class="container">
 								<div class="products__body">
 									<div class="products__column">
@@ -56,8 +56,133 @@
 									</div>
 								</div>
 							</div>
-						</section>
-				 </main>
+					</section>
+					<section class="main__popular popular">
+						<div class="container">
+							<div class="popular__info">
+								<h2 class="popular__title title">Популярная продукция</h2>
+								<p class="popular__text">
+									Подарите своим любимым питомцам тоже счастье, которое 
+									они дарят вам на протяжении всей своей жизни.
+								</p>
+							</div>
+							<div class="popular__body">
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/nordic01.png" alt="nordic01">
+										</div>
+										<h3 class="item-popular__title">Nordic of nature</h3>
+										<p class="item-popular__text">
+											Сбалансированный состав из натуральных 
+											ингредиентов обеспечивает полноценное питание и поддерживает оптимальное 
+											состояние здоровья.
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/nordic02.png" alt="nordic02">
+										</div>
+										<h3 class="item-popular__title">Nordic of nature</h3>
+										<p class="item-popular__text">
+										Каждый кусочек наполнен витаминами, минералами и питательными веществами, 
+										которые помогут вашему питомцу быть активным, сильным и счастливым. 
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/nordic03.png" alt="nordic03">
+										</div>
+										<h3 class="item-popular__title">Nordic of nature</h3>
+										<p class="item-popular__text">
+										Приятный аромат и вкусный вкус делают наш корм незаменимым угощением для вашего пушистого друга.
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/catfood01.png" alt="catfood01">
+										</div>
+										<h3 class="item-popular__title">catfood01</h3>
+										<p class="item-popular__text">
+										Приятный аромат и вкусный вкус делают наш корм незаменимым угощением для вашего пушистого друга.
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/catfood02.png" alt="catfood02">
+										</div>
+										<h3 class="item-popular__title">catfood02</h3>
+										<p class="item-popular__text">
+										Приятный аромат и вкусный вкус делают наш корм незаменимым угощением для вашего пушистого друга.
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+								<div class="popular__column">
+									<div class="popular__item item-popular">
+										<div class="item-popular__image">
+											<img src="img/popular/catfood03.png" alt="catfood03">
+										</div>
+										<h3 class="item-popular__title">catfood03</h3>
+										<p class="item-popular__text">
+										Приятный аромат и вкусный вкус делают наш корм незаменимым угощением для вашего пушистого друга.
+										</p>
+										<div class="item-popular__btn">
+											<a href="#" class="transparent-btn">Посмотреть подробнее</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="popular__all">
+								<a href="#" class="orange-btn">Посмотреть всю продукцию</a>
+							</div>
+						</div>
+					</section>
+					<section class="main__care care">
+						<div class="container">
+							<div class="care__body">
+								<div class="care__left">
+									<img src="img/care/dog-care.png" alt="dog-care">
+								</div>
+								<div class="care__right">
+									<h3 class="care__sub-title">ЗАБОТА О ЖИВОТНЫХ</h3>
+									<h2 class="care__title title">Начните ваш путь с новым питомцем вместе с нами</h2>
+									<p class="care__text">
+										Вы нужны своему питомцу. Чем больше людей вы встречаете, 
+										тем больше любите вашего питомца. Они навсегда оставляют отпечатки
+										своих лап в наших сердцах.
+									</p>
+									<div class="care__buttons buttons">
+											<a href="#" class="care__appointment orange-btn">Встретиться</a>
+											<a href="#" class="care__details transparent-btn">Посмотреть детали</a>
+										</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</main>
          <?php include("./../../components/footer/footer.php") ?>
     </div>
 	</body>
